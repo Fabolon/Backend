1b5ea657f61d0b9923abe57e3a929fc560ce54d2
Fabo <fabianquiro05@hotmail.com>
2025-11-25 21:56:59 -0500

    {Correccion}

--- src/routes/Users.routes.js
+++ src/routes/Users.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { postUser, getAllUsers, putUserById, deleteUserById } from "../controllers/users.controller.js"
+import { postUser, getAllUsers, putUserById, deleteUserById } from "../controllers/Users.controller.js"
 import { auth } from "../middleware/auth.js";
 // 2 Configurar las rutas.
 export const userRouter = express.Router();
